920936205675e81f1890c4d1b0837f5cd065019f
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-18 18:31:17 +0100

    Use test combination to cover all code and conditions

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,9 +12,9 @@ class GildedRoseTest {
   void updateQuality() {
     CombinationApprovals.verifyAllCombinations(
         this::doUpdateQuality,
-        new String[]{"foo"},
-        new Integer[]{0},
-        new Integer[]{0}
+        new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+        new Integer[]{-1, 0, 11},
+        new Integer[]{0, 1, 49, 50}
     );
   }
 
@@ -22,8 +22,7 @@ class GildedRoseTest {
     Item[] items = new Item[]{new Item(name, sellIn, quality)};
     GildedRose app = new GildedRose(items);
     app.updateQuality();
-    String itemString = app.items[0].toString();
-    return itemString;
+    return app.items[0].toString();
   }
 
 }
