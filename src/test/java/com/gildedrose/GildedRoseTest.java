e2ebea7afeea92e4cda654dfc8a4b4e3bd7d749a
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-18 18:17:57 +0100

    update unit test to validate based on toString() method

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,8 +15,7 @@ class GildedRoseTest {
     Item[] items = new Item[]{new Item("foo", 0, 0)};
     GildedRose app = new GildedRose(items);
     app.updateQuality();
-    assertEquals("foo", app.items[0].name);
-    Approvals.verify(app.items[0].name);
+    Approvals.verify(app.items[0].toString());
   }
 
 }
