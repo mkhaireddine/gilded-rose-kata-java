d5daab716d734ce16ce716c6973b8ff5b84a5064
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-19 18:20:04 +0100

    remove unreachable branches in Item class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,61 +11,7 @@ class GildedRose {
   }
 
   public void updateQuality() {
-    Arrays.stream(items).forEach(item -> {
-      switch (item.name) {
-        case "Aged Brie":
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-          }
-          item.sellIn = item.sellIn - 1;
-          if (item.sellIn < 0) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-          break;
-        case "Backstage passes to a TAFKAL80ETC concert":
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-
-          item.sellIn = item.sellIn - 1;
-
-          if (item.sellIn < 0) {
-            item.quality = 0;
-          }
-          break;
-        case "Sulfuras, Hand of Ragnaros":
-          break;
-        default:
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
-
-          item.sellIn = item.sellIn - 1;
-
-          if (item.sellIn < 0) {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1;
-            }
-          }
-          break;
-      }
-
-    });
+    Arrays.stream(items).forEach(Item::updateItemQuality);
   }
 
 }
