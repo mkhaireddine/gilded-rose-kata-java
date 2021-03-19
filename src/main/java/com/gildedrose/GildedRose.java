0091baab755630a18f275872497e8b57d3d87327
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-19 17:54:28 +0100

    simplify conditionals

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -37,17 +37,15 @@ class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
 
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
+          }
 
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
         }
@@ -58,20 +56,14 @@ class GildedRose {
       }
 
       if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
-          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-              }
+        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+              item.quality = item.quality - 1;
             }
-          } else {
-            item.quality = item.quality - item.quality;
           }
         } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+          item.quality = item.quality - item.quality;
         }
       }
 
