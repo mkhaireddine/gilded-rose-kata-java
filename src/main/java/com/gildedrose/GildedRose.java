9c9d50c90c43530bad7d8d992c19379c8a29dce0
KHAIREDDINE Mohamed <mohamed.khaireddine@gmail.com>
2021-03-19 18:00:22 +0100

    end lift up conditional for the 3nd case "Sulfuras"

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,34 +22,30 @@ class GildedRose {
         updateConcert(item);
       } else {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-          foo(item);
+          if (item.quality > 0) {
+          }
+
+          if (item.sellIn < 0) {
+            if (item.quality > 0) {
+            }
+          }
         } else {
-          foo(item);
+          if (item.quality > 0) {
+            item.quality = item.quality - 1;
+          }
+
+          item.sellIn = item.sellIn - 1;
+
+          if (item.sellIn < 0) {
+            if (item.quality > 0) {
+              item.quality = item.quality - 1;
+            }
+          }
         }
       }
 
     }
 
-  }
-
-  private void foo(Item item) {
-    if (item.quality > 0) {
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        item.quality = item.quality - 1;
-      }
-    }
-
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      item.sellIn = item.sellIn - 1;
-    }
-
-    if (item.sellIn < 0) {
-      if (item.quality > 0) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-          item.quality = item.quality - 1;
-        }
-      }
-    }
   }
 
   private void updateConcert(Item item) {
